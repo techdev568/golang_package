b312ed1c32eeb2ac4b6157b1886de2cc9a812e4f
techdev568 <techdev568@gmail.com>
2023-06-13 08:13:21 +0530

    added maths package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/techdev568/golang_package
+
+go 1.19
---
+++ maths.go
@@ -0,0 +1,20 @@
+package maths
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Sub(a int, b int) int {
+	return a - b
+}
+
+func Mul(a int, b int) int {
+	return a * b
+}
+
+
+// Any project we required logging ->
+
+//Steps happening in your pro
+
+// just msg , time stamp : user loged in 
